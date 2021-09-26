7bf99f51fdebcda14a40d32755ee77570663fdf8
theaniartist <wsly@wpi.edu>
2021-09-26 14:40:38 -0400

    Add all audio buttons and functionality working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,32 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const overlay = document.getElementById('overlay')
+const playBtn = document.getElementById('play')
+
+const sajouBtn = document.getElementById('sajou'),
+      sawanoBtn = document.getElementById('sawano'),
+      reonaBtn = document.getElementById('reona'),
+      lisaBtn = document.getElementById('lisa'),
+      utadaBtn = document.getElementById('utada'),
+      sasanomalyBtn = document.getElementById('sasanomaly'),
+      aimerBtn = document.getElementById('aimer')
+
+let color = 'white';
+
 const sajou = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%20Gray.mp3?v=1632606001734"
 const sawano = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F02.Hands%20Up%20to%20the%20Sky.mp3?v=1632612774842"
 const reona = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F03.Untitled%20world.mp3?v=1632613620178"
+const lisa = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%20%E7%B4%85%E8%93%AE%E8%8F%AF.mp3?v=1632679415882"
+const utada = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.PINK%20BLOOD.mp3?v=1632679673420"
+const sasanomaly = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%E7%A9%BA%E3%81%A8%E8%99%9A.mp3?v=1632680283648"
+const aimer = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F04%203min.mp3?v=1632680298458"
+
+//audio init
+const audioCtx = new AudioContext()
+const audioElement = document.createElement('audio')
+document.body.appendChild(audioElement)
 
 const start = function () {
-
-    document.body.innerHTML = ''
-    const canvas = document.createElement( 'canvas' )
-    document.body.appendChild( canvas )
-    canvas.width = canvas.height = 512
-    const ctx = canvas.getContext( '2d' )
-
-    //audio init
-    const audioCtx = new AudioContext()
-    const audioElement = document.createElement('audio')
-    document.body.appendChild(audioElement)
 
     //audio graph setup
     const analyser = audioCtx.createAnalyser()
@@ -37,20 +50,59 @@ const start = function () {
         ctx.fillStyle = 'black'
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-        // set the color to white for drawing our visuaization
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = color
 
         analyser.getByteFrequencyData(results)
 
         for (let i = 0; i < analyser.frequencyBinCount; i++) {
-            ctx.fillRect(i, 0, 1, results[i]) // upside down!
+            ctx.fillRect(i, canvas.height, 3, -results[i])
         }
     }
-
     draw()
 }
 
-window.onload = () => document.getElementById('reona').onclick = start
+playBtn.addEventListener('click', function() {
+    audioCtx.resume()
+    start()
+})
 
+sajouBtn.addEventListener('click', function() {
+    audioElement.src = sajou
+    audioElement.play()
+})
 
+sawanoBtn.addEventListener('click', function() {
+    audioElement.src = sawano
+    audioElement.play()
+})
 
+reonaBtn.addEventListener('click', function() {
+    audioElement.src = reona
+    audioElement.play()
+})
+
+lisaBtn.addEventListener('click', function() {
+    audioElement.src = lisa
+    audioElement.play()
+})
+
+utadaBtn.addEventListener('click', function() {
+    audioElement.src = utada
+    audioElement.play()
+})
+
+sasanomalyBtn.addEventListener('click', function() {
+    audioElement.src = sasanomaly
+    audioElement.play()
+})
+
+aimerBtn.addEventListener('click', function() {
+    audioElement.src = aimer
+    audioElement.play()
+})
+
+const blue = document.getElementById('blueGradient')
+
+blue.addEventListener('click', function () {
+    color = "#7DC6E0";
+})
--- server.js
+++ server.js
@@ -14,3 +14,5 @@ app.get("/", (request, response) => {
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+app.listen(3000)
