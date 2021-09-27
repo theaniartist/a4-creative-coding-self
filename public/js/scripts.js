b1fe5bb6487753568ffc5c0abf5dce1a26fa8aeb
theaniartist <wsly@wpi.edu>
2021-09-26 20:21:32 -0400

    Add and swap songs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,23 +5,25 @@ const content = document.getElementById('content')
 const title = document.getElementById('title')
 const playBtn = document.getElementById('play')
 
-const sajouBtn = document.getElementById('sajou'),
+const hatenaBtn = document.getElementById('hatena'),
       sawanoBtn = document.getElementById('sawano'),
       reonaBtn = document.getElementById('reona'),
       lisaBtn = document.getElementById('lisa'),
       utadaBtn = document.getElementById('utada'),
       sasanomalyBtn = document.getElementById('sasanomaly'),
-      aimerBtn = document.getElementById('aimer')
+      aimerBtn = document.getElementById('aimer'),
+      lynBtn = document.getElementById('lyn')
 
 let color = 'white';
 
-const sajou = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%20Gray.mp3?v=1632606001734"
+const hatena = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%E5%A4%A2.mp3?v=1632698467767"
 const sawano = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F02.Hands%20Up%20to%20the%20Sky.mp3?v=1632612774842"
 const reona = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F03.Untitled%20world.mp3?v=1632613620178"
 const lisa = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%20%E7%B4%85%E8%93%AE%E8%8F%AF.mp3?v=1632679415882"
 const utada = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.PINK%20BLOOD.mp3?v=1632679673420"
 const sasanomaly = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F01.%E7%A9%BA%E3%81%A8%E8%99%9A.mp3?v=1632680283648"
 const aimer = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2F04%203min.mp3?v=1632680298458"
+const lyn = "https://cdn.glitch.com/1be4c9fb-3024-481f-be52-d8a904df0358%2Fdisc1_03.mp3?v=1632698430615"
 
 //audio init
 const audioCtx = new AudioContext()
@@ -37,7 +39,7 @@ const start = function () {
     player.connect(audioCtx.destination)
     player.connect(analyser)
 
-    audioElement.src = sajou
+    audioElement.src = hatena
     audioElement.play()
     audioElement.crossOrigin = "anonymous";
 
@@ -68,8 +70,8 @@ playBtn.addEventListener('click', function() {
     start()
 })
 
-sajouBtn.addEventListener('click', function() {
-    audioElement.src = sajou
+hatenaBtn.addEventListener('click', function() {
+    audioElement.src = hatena
     audioElement.play()
 })
 
@@ -100,6 +102,11 @@ sasanomalyBtn.addEventListener('click', function() {
 
 aimerBtn.addEventListener('click', function() {
     audioElement.src = aimer
+    audioElement.play()
+})
+
+lynBtn.addEventListener('click', function() {
+    audioElement.src = lyn
     audioElement.play()
 })
 
